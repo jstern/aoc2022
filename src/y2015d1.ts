f4e517b6614eed279331f81e0f1e617bd91baeb0
Jon Stern <jonathan.a.stern@gmail.com>
2022-11-27 15:59:03 -0600

    2015 day 1 part 2

--- src/y2015d1.ts
+++ src/y2015d1.ts
@@ -1,10 +1,44 @@
 import { chars } from './util.js';
 
-export function part1(input: string): string {
-  let floor = 0;
-  chars(input).forEach((c) => {
-    if (c == '(') floor++;
-    if (c == ')') floor--;
+const Up = '(';
+const Dn = ')';
+type Move = typeof Up | typeof Dn;
+
+type State = {
+  pos: number;
+  flr: number;
+};
+
+function next(cur: State, mov: Move): State {
+  if (mov == Up) {
+    return { pos: cur.pos + 1, flr: cur.flr + 1 };
+  }
+  return { pos: cur.pos + 1, flr: cur.flr - 1 };
+}
+
+type ContinueCheck = (s: State) => boolean;
+
+function NoStop(_: State): boolean {
+  return true;
+}
+
+function process(input: string, check: ContinueCheck = NoStop): State {
+  let state = { pos: 0, flr: 0 };
+  chars(input).every((c) => {
+    state = next(state, c as Move);
+    return check(state);
   });
-  return floor.toString();
+  return state;
+}
+
+export function part1(input: string): string {
+  return process(input).flr.toString();
+}
+
+function NotInBasement(s: State): boolean {
+  return s.flr >= 0;
+}
+
+export function part2(input: string): string {
+  return process(input, NotInBasement).pos.toString();
 }
