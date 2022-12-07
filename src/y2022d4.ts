57a086f8f3dbcec45835fe4214b49d180a8a7554
Jon Stern <jonathan.a.stern@gmail.com>
2022-12-07 07:59:20 -0600

    day 4

---
+++ src/y2022d4.ts
@@ -0,0 +1,28 @@
+import { lines } from "./util.js";
+
+type range = [number, number];
+
+function parseRange(desc: string): range {
+    return desc.split('-').map(v => parseInt(v)) as range;
+}
+
+function testContains(desc: string): boolean {
+    const [r1, r2]= desc.split(',').map(v => parseRange(v));
+    return (r1[0] <= r2[0] && r1[1] >= r2[1]) || (r2[0] <= r1[0] && r2[1] >= r1[1]);
+}
+
+export function part1(input: string): number {
+    return lines(input).map(l => testContains(l)).filter(v => v).length;
+}
+
+function testOverlaps(desc: string): boolean {
+    const [r1, r2]= desc.split(',').map(v => parseRange(v));
+    return (
+        (r1[1] >= r2[0] && r1[0] >= r2[0] && r1[0] <= r2[1]) ||
+        (r2[1] >= r1[0] && r2[0] >= r1[0] && r2[0] <= r1[1])
+    );
+}
+
+export function part2(input: string): number {
+    return lines(input).map(l => testOverlaps(l)).filter(v => v).length;
+}
