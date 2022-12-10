3aab43bf63b4997fafdc70854a9e4df8e760200d
Jon Stern <jonathan.a.stern@gmail.com>
2022-12-10 11:54:53 -0600

    day 6

---
+++ src/y2022d6.ts
@@ -0,0 +1,29 @@
+import { chars } from "./util.js";
+
+function updateWindow(size: number, window: string[], c: string): void {
+  window.push(c);
+  if (window.length > size) { window.shift(); }
+}
+
+function startsSignal(size: number, window: string[]): boolean {
+  return window.length == size && new Set(window).size == size;
+}
+
+function findStartOfSignal(size: number, msg: string): number {
+  const window = new Array<string>();
+  let i = 0;
+  chars(msg).some((c) => {
+    i += 1;
+    updateWindow(size, window, c);
+    return startsSignal(size, window);
+  });
+  return i;
+}
+
+export function part1(input: string): number {
+  return findStartOfSignal(4, input);
+}
+
+export function part2(input: string): number {
+  return findStartOfSignal(14, input);
+}
